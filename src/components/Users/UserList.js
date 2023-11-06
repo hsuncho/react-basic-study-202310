5c95aba6d580fcd9e83fe6dbb2039bde3daf049e
hyeonseon <hsuncho@naver.com>
2023-11-06 10:38:49 +0900

    에러 모달 만들기

---
+++ src/components/Users/UserList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './UserList.module.css';
+import Card from '../UI/Card';
+
+const UserList = ({ users }) => {
+  console.log(users);
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {users.map(
+          (
+            user //return 문 대신 ()
+          ) => (
+            <li key={user.id}>
+              {user.userName} ({user.age}세)
+            </li>
+          )
+        )}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;

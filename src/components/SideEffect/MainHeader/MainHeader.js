5ef950bd21ca88b3feb150fa02967845b7514c4c
hyeonseon <hsuncho@naver.com>
2023-11-08 12:36:21 +0900

    useContext와 리팩토링

--- src/components/SideEffect/MainHeader/MainHeader.js
+++ src/components/SideEffect/MainHeader/MainHeader.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Navigation from '../Navigation/Navigation';
 import classes from './MainHeader.module.css';
 
-const MainHeader = ({ onLogout }) => {
+const MainHeader = () => {
   // data dstructuring (also available to get data using props)
   return (
     <header className={classes['main-header']}>
       <h1>A Typical Page</h1>
-      <Navigation onLogout={onLogout} />
+      <Navigation />
     </header>
   );
 };
--- src/components/SideEffect/Navigation/Navigation.js
+++ src/components/SideEffect/Navigation/Navigation.js
@@ -1,38 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import classes from './Navigation.module.css';
 import AuthContext from '../../../store/auth-context';
 
 // menu(decide to show li elements if IsLoggedIn = true)
-const Navigation = ({ onLogout }) => {
-  // get onLogout function from MainHeader
+const Navigation = () => {
+  const { isLoggedIn, onLogout } = useContext(AuthContext);
+
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        // isLoggedIn will come from provider
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {context.isLoggedIn && (
-                <li>
-                  <a href='/'>Users</a>
-                </li>
-              )}
-              {context.isLoggedIn && (
-                <li>
-                  <a href='/'>Admin</a>
-                </li>
-              )}
-              {context.isLoggedIn && ( // execute logoutHandler
-                <li>
-                  <button onClick={onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {isLoggedIn && (
+          <li>
+            <a href='/'>Users</a>
+          </li>
+        )}
+        {isLoggedIn && (
+          <li>
+            <a href='/'>Admin</a>
+          </li>
+        )}
+        {isLoggedIn && ( // execute logoutHandler
+          <li>
+            <button onClick={onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
